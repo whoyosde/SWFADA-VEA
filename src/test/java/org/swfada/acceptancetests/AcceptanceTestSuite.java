b33c9f5547f02a28770b97a2f65b00cbdfb90eea
Walterhd30 <walterhd30@gmail.com>
2026-01-26 16:07:28 -0500

    primer commit

---
+++ src/test/java/org/swfada/acceptancetests/AcceptanceTestSuite.java
@@ -0,0 +1,21 @@
+package org.swfada.acceptancetests;
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        plugin = {"html:target/build/cucumber-html-report"
+                , "pretty:target/build/cucumber-pretty.txt"
+                , "json:target/build/cucumber.json"},
+        stepNotifications = true,
+        features = "src/test/resources/Features",
+        glue = "org.swfada",
+        tags ="@PF014"
+
+)
+
+public class AcceptanceTestSuite {
+}
+
---
+++ src/test/resources/Features/PF014.feature
@@ -0,0 +1,10 @@
+#language: es
+  @PF014
+  Característica: PF014 - Acceder a procedimiento desde búsqueda de procedimientos predictiva
+    Como usuario registrado en el sistema
+    Quiero ingresar a la plataforma Vea Cloud
+    para realizar una búsqueda predictiva
+  Antecedentes:
+    Dado que el usuario ingresa a la plataforma Vea Cloud
+    Y acepto los Cookies
+
